2e3a336aa10d0b19d18b0ce93f9ac9f599122b76
Guilherme Marques <marquesguilherme3@gmail.com>
2018-08-21 00:45:12 -0300

    Starting binary search

--- algorithms.py
+++ algorithms.py
@@ -10,6 +10,7 @@ class Search(object):
     self.array = array
     self.index_table = []
     self.updated = False
+    self.ordered = False
   
   def _search_menu(self):
     print('1 - Busca sequencial')
@@ -40,6 +41,13 @@ class Search(object):
         entry = int(input('Insira um valor: '))
         start = time.process_time()
         print('Resultado: ' + str(self.seq_transp(entry)))
+        end = time.process_time()
+        print('Tempo gasto: ' + str(end - start))
+      elif opt == '4':
+        clear()
+        entry = int(input('Insira um valor: '))
+        start = time.process_time()
+        print('Resultado: ' + str(self.bin_search(entry)))
         end = time.process_time()
         print('Tempo gasto: ' + str(end - start))
       elif opt == '0':
@@ -81,17 +89,50 @@ class Search(object):
       Implementa uma busca sequencial com método da transposição.
       Cada elemento buscado é trocado com o elemento 5 posições anterior na lista.
       Caso o valor não exista na lista, retorna -1.
+      Caso exista retorna o índice.
     """
     result = -1
     for index, val in enumerate(self.array):
       if val == value:
         if index > 5:
           self.updated = True
+          self.ordered = False
           self.array[index], self.array[index - 5] = self.array[index - 5], self.array[index]
           index -= 5
         result = index
         break
     
     return result
+
+  def _bin(self, value, min, max, l):
+    """
+      Função auxiliar que implementa o algoritmo da busca binária.
+    """
+    l = [l]
+    print(l)
+    if min == max:
+      return -1
+    mid = int(min + max / 2)
+    if l[mid][1] == value:
+      return l[mid][1]
+    elif l[mid][1] < value:
+      return self._bin(value, mid, max, l)
+    else:
+      return self._bin(value, min, mid, l)
+
+  def bin_search(self, value):
+    """
+      Implementa uma busca binária.
+      Retorna o índice do valor procurado caso exista na lista.
+      Retorna -1 caso não exista.
+    """
+    if not self.ordered:
+      self.updated = True
+      self.ordered = True
+      self.array.sort()
+    
+    return self._bin(value, 0, (len(self.array) - 1), [enumerate(self.array)])
+    
+
 
 
